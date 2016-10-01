d65b681604b89d940c96f245cc94dffedca89c03
thomas <scottrobbertson@gmail.com>
2016-10-01 18:52:03 -0500

    change files

--- items/item.py
+++ items/item.py
@@ -1,5 +1,4 @@
 class Item:
-
     def __init__(self, name: str, description: str, value: int) -> object:
         self.Name = name
         self.Description = description
@@ -11,7 +10,6 @@ class Item:
 
 
 class Container(Item):
-
     def __init__(self, name, description, value, contents):
         """
         :type name: str
@@ -27,20 +25,21 @@ class Container(Item):
     def __str__(self):
         contents = ""
         for thing in self.Contents:
+            assert isinstance(thing, Item)
             contents += "\n" + thing.__str__()
 
         return str(super(Container, self).__str__()) + contents
 
 
 class Weapon(Item):
-
     def __init__(self, name, description, value, damage):
         super(Weapon, self).__init__(name, description, value)
+        assert isinstance(damage, int)
         self.Damage = damage
 
 
 class Shield(Item):
-
     def __init__(self, name, description, value, defense):
         super(Shield, self).__init__(name, description, value)
+        assert isinstance(defense, int)
         self.Defense = defense
