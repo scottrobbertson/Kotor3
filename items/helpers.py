62c4c0f401b9596d20371918dc49c27ac20c4dce
thomas <scottrobbertson@gmail.com>
2016-10-01 18:35:24 -0500

    add file

--- items/helpers.py
+++ items/helpers.py
@@ -1,4 +1,12 @@
+from items.item import *
+
+
 def sell(item, store, player):
+    """
+    :param item A item the player wants to sell
+    :param store The place that wants to buy the item
+    :param player The player selling the item
+    """
     if item in player.inventory:
         store.inventory += item
         player.inventory -= item
@@ -6,6 +14,8 @@ def sell(item, store, player):
 
 
 def buy(item, player, store):
+    assert isinstance(item, Item)
+    assert isinstance(item.Value, int)
     if item in store.inventory:
         store.inventory -= item
         player.inventory += item
